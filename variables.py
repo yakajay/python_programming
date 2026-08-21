81a35f4e66c911b271eaf2c1129efad8acd4a515
Yakkali Ajay Kumar <102503364+yakajay@users.noreply.github.com>
2026-08-22 02:25:43 +0530

    String, int data types and print the values in multiples lines

--- variables.py
+++ variables.py
@@ -8,3 +8,41 @@ print(message)
 username = "Ajay"
 
 print(username)
+
+# Data types
+
+# Text Type = str Strinf
+
+username = "Ajay"
+
+print(type(username)) # String Data Type
+
+# Numeric Data types = int, float, complex
+
+phone_number = 9505959888
+
+print(type(phone_number)) # Integer Data Type
+
+cities = '''Hyderabad, 
+Secunderabad, 
+Vijayawada''' # if we want the print the statement in multiple lines we have to pass like ''' / """ this to print the stament in multi lines
+
+print(cities)
+
+countries = """India,
+China,
+USA"""
+
+print(countries)
+
+# Sequence Data type = list, tuple, rane
+
+# Mapping Data Type = dict
+
+# Set Data Type = set, frozenset
+
+# Boolean Data Type = bool
+
+# Binary Data Type = bytes, bytearray, memoryview
+
+# None Data Type = NoneType
