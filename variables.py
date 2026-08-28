f8045f8e66db4593c470fbb47d240504506427d9
Yakkali Ajay Kumar <102503364+yakajay@users.noreply.github.com>
2026-08-28 15:12:51 +0530

    nested loop

--- variables.py
+++ variables.py
@@ -365,3 +365,17 @@ for col in colour:
 
 for item in range(5):
     print(item)
+
+# Nested Loop
+
+rows = 10
+
+for i in range(1, rows+1):
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print()
+
+for i in range(rows, 0, -1):
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print()
