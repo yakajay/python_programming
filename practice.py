e888e88f91daa653798262e4d6fccbd7d7186d4f
Yakkali Ajay Kumar <102503364+yakajay@users.noreply.github.com>
2026-08-29 22:16:50 +0530

    29-Aug practice

--- practice.py
+++ practice.py
@@ -44,3 +44,16 @@ for newcol in colors_new:
 
 for i in range(20):
     print(i)
+
+
+rows = 10
+
+for i in range(1, rows+1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
+
+for i in range(rows, 0, - 1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
