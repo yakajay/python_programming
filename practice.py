7f955a4195cb48bd246402e5e3a70c6a5914f286
Yakkali Ajay Kumar <102503364+yakajay@users.noreply.github.com>
2026-08-30 19:18:10 +0530

    nested loop concetps

--- practice.py
+++ practice.py
@@ -57,3 +57,16 @@ for i in range(rows, 0, - 1):
     for j in range(1, i + 1):
         print("*", end=" ")
     print()
+
+
+rows = 10
+
+for a in range(0, rows + 1):
+    for b in range(0, a + 1):
+        print("*", end=" ")
+    print()
+
+for a in range(rows, 0, -1):
+    for b in range(0, a + 1):
+        print("*", end=" ")
+    print()
