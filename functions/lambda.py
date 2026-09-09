994af4dc7da2e7e8a03e7ca21e6f49f64ad803ae
Yakkali Ajay Kumar <102503364+yakajay@users.noreply.github.com>
2026-09-09 18:02:39 +0530

    callback functions

--- functions/lambda.py
+++ functions/lambda.py
@@ -22,3 +22,20 @@ calc_la = lambda x, y: x + y # taking the arguments and giving a calculation.
 
 print(calc_la(10, 20))
 
+# call bacl function => callback functions in python are functions passed as arguments to other functions They are executed when the operation of the function completes.
+
+products = ("mobiles", "laptops", "tablets")
+
+print(products)
+
+def mobile_price():
+    print("price of mobile is 15000")
+
+def process_selection(selected_item, product_price):
+    if selected_item == "mobiles":
+        product_price()
+
+def select_product():
+    selected_item = input("select your product: ")
+    process_selection(selected_item, mobile_price)
+select_product()
